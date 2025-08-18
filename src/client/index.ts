d6a376221326ebf0aee2d06fa0554f6f9e4fec3b
Filipe <filipe.so137@gmail.com>
2025-08-18 19:47:22 -0300

    colisao funcionado

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,19 +1,134 @@
 // import { server } from "../server/server";
 import { io } from 'socket.io-client';
 
+/*
+ver sobre chunks
+carregar objs conforme aparece na camera
+colisoes com ambinte
+minimapa, mundo mo limite
 
+*/
 
 
 export class index {
 
     socket = io("http://localhost:3000");
+
+    //player
     name?: string;
     id?: string;
 
-    init() {
+    //area
+    dpr: number = window.devicePixelRatio || 1; //valor repsentante do pixel real
+    steps: number = 1;
+    canvas: HTMLCanvasElement = document.getElementById("area") as HTMLCanvasElement;
+    ctx?: CanvasRenderingContext2D;
+    width: number = 0;
+    height: number = 0;
+    worldx: number = 0;
+    worldy: number = 0;
+    keys: any = {};
+    keyCollision?: string;
+    myPlayer: any = {
+        size: 20,
+        color: "red"
+    }
+    objects: any[] = [
+        { x: 20, y: 10, size: 20, color: "green", img: "tree.gif" },
+        { x: -150, y: 50, size: 55, color: "blue", img: "tree.gif" },
+        { x: 300, y: -200, size: 80, color: "gray", img: "tree.gif" },
+        { x: 100, y: 300, size: 130, color: "orange", img: "tree.gif" },
+        { x: -200, y: -150, size: 10, color: "black", img: "tree.gif" },
+    ];
+
+    img = new Image();
+            
+
+    //keys
+    delayKeyPress: number = 1000;
+    lastKeyPress: number = 0;
+    arrowKeys: any = {
+        up: "ArrowUp",
+        down: "ArrowDown",
+        left: "ArrowLeft",
+        right: "ArrowRight"
+    }
+
+
+
+    constructor() {
+        this.addlisteners();
+        this.createCanvas();
+        this.resizeCanvas();
+        this.initSocket();
+        this.gameLoop();
+    }
+
+
+    createCanvas() {
+        // let canvas = document.getElementById("area") as HTMLCanvasElement;
+        // this.ctx = canvas.getContext("2d") || undefined;
+
+        this.ctx = this.canvas.getContext("2d") || undefined;
+
+    }
+
+    resizeCanvas() {
+        this.width = this.canvas.clientWidth;
+        this.height = this.canvas.clientHeight;
+        this.canvas.width = this.width * this.dpr;
+        this.canvas.height = this.height * this.dpr;
+        this.canvas.style.width = this.width + "px";
+        this.canvas.style.height = this.height + "px";
+    }
+
+    drawObjects() {
+        // type GameObject = {
+        // x: number;
+        // y: number;
+        // color: string;
+        // };
+// this.img.src = 'https://via.placeholder.com/200x150';
+        this.ctx?.clearRect(0, 0, this.width, this.height);
+        if (this.name) {
+            this.createPlayer();
+        }
+        this.objects.forEach((obj) => {
+
+            let screenX = obj.x - this.worldx;
+            let screenY = obj.y - this.worldy;
+
+            //            // Desenha só se estiver dentro do canvas
+            // if (
+            //   screenX + 32 < 0 || screenX > WIDTH ||
+            //   screenY + 32 < 0 || screenY > HEIGHT
+            // ) {
+            //   continue;
+            // }
+
+            // let img = new Image();
+            // img.src = 'https://via.placeholder.com/200x150';
+            // img.src = './tree.gif';
+            // this.img.onload = () => {
+
+            //     this.ctx!.drawImage(this.img, screenX, screenY, obj.size, obj.size);
+
+
+            //     this.ctx!.strokeStyle = 'black';
+            //     this.ctx!.lineWidth = 2;
+            //     this.ctx!.strokeRect(screenX, screenY,  obj.size,  obj.size);
+            // }
+            this.ctx!.fillStyle = obj.color;
+            this.ctx!.fillRect(screenX, screenY, obj.size, obj.size);
+        });
+    }
+
+
+    initSocket() {
         // let s = new server();
         // // s.listen();
         // s.init();
+
         console.log("iniciou client");
 
         this.socket.on('connection', (name: string) => {
@@ -49,48 +164,51 @@ export class index {
     addlisteners() {
         document.getElementById("join")?.addEventListener("click", () => this.joinGame());
         document.getElementById("action")?.addEventListener("click", () => this.sendAction());
+        document.addEventListener("resize", this.resizeCanvas);
     }
 
     addListenersControls(id: string) {
         document.addEventListener("keydown", (event) => {
-            switch (event.key) {
-                case "ArrowUp":
-                    this.movePlayer(this.id!, "up");
-                    break;
-                case "ArrowDown":
-                    this.movePlayer(this.id!, "down");
-                    break;
-                case "ArrowLeft":
-                    this.movePlayer(this.id!, "left");
-                    break;
-                case "ArrowRight":
-                    this.movePlayer(this.id!, "right");
-                    break;
+            console.log("key");
+            let now = Date.now();
+            if (now - this.lastKeyPress < this.delayKeyPress) { return; }
+            this.keys[event.key] = true;
+        });
+        document.addEventListener("keyup", (event) => {
+            this.keys[event.key] = false;
+        });
+    }
+
+    collisionPlayer(key: string, futurePosition: number) {
+        let left = 0, right = 0, up = 0, down = 0;
+        if (key == this.arrowKeys.left) { left = futurePosition }
+        if (key == this.arrowKeys.right) { right = futurePosition; }
+        if (key == this.arrowKeys.up) { up = futurePosition; }
+        if (key == this.arrowKeys.down) { down = futurePosition; }
+        let halfPlayer = this.myPlayer.size / 2;
+
+        return this.objects.some((e) => {
+
+            if ((e.x - this.worldx) > 0 && (e.x - this.worldx) < this.width
+                && (e.y - this.worldy) > 0 && (e.y - this.worldy) < this.height) {
+
+                if (((e.x - this.worldx) + e.size + halfPlayer) >= ((this.width * this.dpr) / 2) - left
+                    && (e.x - this.worldx) <= (((this.width * this.dpr) / 2) + halfPlayer + right)
+                    && ((e.y - this.worldy) + e.size + halfPlayer) >= ((this.height * this.dpr) / 2) - up
+                    && (e.y - this.worldy) <= (((this.height * this.dpr) / 2) + halfPlayer + down)) {
+
+                    return true;
+                }
             }
         });
     }
 
-    movePlayer(id: string, direction: string) {
-        let player = document.getElementById(id);
-        let top = parseInt(player!.style.top) || 0;
-        let left = parseInt(player!.style.left) || 0;
-        switch (direction) {
-            case "up":
-                if (id == this.id) this.socket.emit("movePlayer", { id: id, direction: "up" });
-                player!.style.top = top - 10 + "px";
-                break;
-            case "down":
-                if (id == this.id) this.socket.emit("movePlayer", { id: id, direction: "down" });
-                player!.style.top = top + 10 + "px";
-                break;
-            case "left":
-                if (id == this.id) this.socket.emit("movePlayer", { id: id, direction: "left" });
-                player!.style.left = left - 10 + "px";
-                break;
-            case "right":
-                if (id == this.id) this.socket.emit("movePlayer", { id: id, direction: "right" });
-                player!.style.left = left + 10 + "px";
-                break;
+    movePlayer(id?: string, direction?: any) {
+        if (Object.keys(this.keys).length > 0) {
+            if (this.keys[this.arrowKeys.up] && !(this.collisionPlayer(this.arrowKeys.up, 1))) { this.worldy -= this.steps; }
+            if (this.keys[this.arrowKeys.left] && !(this.collisionPlayer(this.arrowKeys.left, 1))) { this.worldx -= this.steps; }
+            if (this.keys[this.arrowKeys.down] && !this.collisionPlayer(this.arrowKeys.down, 1)) { this.worldy += this.steps; }
+            if (this.keys[this.arrowKeys.right] && !this.collisionPlayer(this.arrowKeys.right, 1)) { this.worldx += this.steps; }
         }
     }
 
@@ -114,6 +232,22 @@ export class index {
         this.socket.emit("playerAction", `${this.name} fez uma acao`);
     }
 
+    createPlayer(id?: string) {
+        if (this.ctx) {
+            this.ctx.fillStyle = "red";
+            // this.ctx.fillRect((this.width / 2 - 10), (this.height / 2 - 10), 20, 15);
+            this.ctx.fillRect(((this.width * this.dpr) / 2) - (this.myPlayer.size / 2), ((this.height * this.dpr) / 2) - (this.myPlayer.size / 2), this.myPlayer.size, this.myPlayer.size);
+        }
+
+
+        // let p = document.createElement("div");
+        // p.style.width = "20px";
+        // p.style.height = "20px";
+        // p.style.backgroundColor = "red";
+        // p.style.position = "absolute";
+        // p.id = id;
+        // document.getElementById("area")?.appendChild(p);
+    }
 
     log(message: string) {
         let li = document.createElement("li");
@@ -121,20 +255,18 @@ export class index {
         document.getElementById("logs")?.appendChild(li);
     }
 
-    createPlayer(id: string) {
-        let p = document.createElement("div");
-        p.style.width = "20px";
-        p.style.height = "20px";
-        p.style.backgroundColor = "red";
-        p.style.position = "absolute";
-        p.id = id;
-        document.getElementById("content")?.appendChild(p);
+    private gameLoop = () => {
+        this.movePlayer();
+        this.drawObjects();
+        // this.initSocket();
+        // this.addlisteners();
+        requestAnimationFrame(this.gameLoop);
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     const i = new index();
-    i.init();
-    i.addlisteners();
+
+
 
 });
